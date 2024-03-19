0680e93822a986543b03227466259a87bed0fff3
Matti <mattihro105@gmail.com>
2024-03-19 16:18:31 +0100

    added favicon and icons

--- eureostix/tix/forms.py
+++ eureostix/tix/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class createTicketForm(forms.Form):
     name = forms.CharField(label='Vor- und Nachname', max_length=255)
     mail_adress = forms.CharField(label="Ihre eureos-Mail-Adresse", max_length=255)
-    CHOICES = (('Windows', 'Windows'),('Handy', 'Handy'),('DATEV', 'DATEV'), ('Microsoft Office', 'Microsoft Office'), ('Hardwareprobleme', 'Hardwareprobleme'))
+    CHOICES = (('Windows', 'Windows'),('Handy', 'Handy'),('DATEV', 'DATEV'), ('Microsoft Office', 'Microsoft Office'), ('Hardwareprobleme', 'Hardwareprobleme'), ('Remote-Work / VPN', 'Remote-Work / VPN'), ('ELO', 'ELO'), ('Anderes', 'Anderes'))
     problem = forms.ChoiceField(choices=CHOICES)
     title = forms.CharField(label="Betreff des Tickets", max_length=255)
     description = forms.CharField(label="Bitte beschreiben Sie ihr Problem", widget=forms.Textarea(), max_length=2000)
