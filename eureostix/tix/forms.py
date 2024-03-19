971b29bd4ec1788620149f98214076fe2f52cf9a
Matti <mattihro105@gmail.com>
2024-03-19 16:46:37 +0100

    änderungen

--- eureostix/tix/forms.py
+++ eureostix/tix/forms.py
@@ -7,7 +7,7 @@ class createTicketForm(forms.Form):
     CHOICES = (('Windows', 'Windows'),('Handy', 'Handy'),('DATEV', 'DATEV'), ('Microsoft Office', 'Microsoft Office'), ('Hardwareprobleme', 'Hardwareprobleme'), ('Remote-Work / VPN', 'Remote-Work / VPN'), ('ELO', 'ELO'), ('Anderes', 'Anderes'))
     problem = forms.ChoiceField(choices=CHOICES)
     title = forms.CharField(label="Betreff des Tickets", max_length=255)
-    description = forms.CharField(label="Bitte beschreiben Sie ihr Problem", widget=forms.Textarea(), max_length=2000)
+    description = forms.CharField(label="Bitte beschreiben Sie ihr Problem", widget=forms.Textarea(), max_length=20000)
 
 
 class addMessage(forms.Form):
